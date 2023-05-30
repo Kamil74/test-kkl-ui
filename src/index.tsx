7e28f87501a153de963a21314d303a36db259a68
Rafael Milewski <rafimilewski@hotmail.com>
2023-05-30 14:18:23 +0300

    first fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-// @ts-ignore
 import React from 'react';
-// @ts-ignore
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,18 +9,19 @@ import './mocks/browser';
 LicenseInfo.setLicenseKey('06e3841ea082fb29f149cb8cca0ebb71Tz01NjkwNixFPTE3MDQxODk2MDUyNTMsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI=');
 
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
+    const { worker } = require('./mocks/browser');
+    worker.start();
 }
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/settings/errors.ts
@@ -0,0 +1,29 @@
+export const errors = {
+    'Auth.InvalidCred': 'תעודת זהות או טלפון שגויים',
+    'Auth.UserNotFound': 'משתמש לא נמצא',
+    'Auth.ValidationFailure': 'אימות נכשל',
+
+    'Events.NotAuthorized': 'אין הרשאה לפעולה זאת',
+    'Events.EventNotFound': 'אירוע לא נמצא',
+    'School.SchoolNotFound': 'בית ספר לא נמצא',
+
+    'User.DuplicateEmail': 'דוא"ל כבר קיים',
+    'User.UserNotFound': 'משתמש לא נמצא',
+    'User.DuplicateIdNumber': 'תעודת זהות כבר קיימת',
+    'User.DuplicatePhoneNumber': 'מספר טלפון כבר קיים',
+    'User.UnauthorizedRequest': 'אין הרשאה לפעולה זאת',
+    'User.SchoolNotFound': 'לא נמצא בית ספר עבור משתמש זה',
+    'User.UserAlreadyInSchool': 'המשתמש כבר משויך לבית ספר זה',
+    'User.UnbelievableError': 'געוואלד! אירעה שגיאה בלתי ניתנת להסבר',
+    'User.UserNotInSchool': 'המשתמש אינו משויך לבית ספר זה',
+
+    'SchoolStats.WrongFormat': 'הקובץ בפורמט שגוי',
+    'SchoolStats.ActivityNotFound': 'נמצאה פעילות לא ידועה למערכת',
+    'SchoolStats.SchoolStatsNotFound': 'לא נמצאו סטטיסטיקות לבית ספר זה',
+    'SchoolStats.EmailNotSent': 'המייל לא נשלח',
+
+    'instructions.NoFilesWasUploaded': 'לא הועלו קבצים',
+    'instructions.FilesUploadError': 'שגיאה בהעלאת קבצים',
+    'instructions.FailedToMoveFile': 'שגיאה בהעברת קובץ',
+    'instructions.FileNotFound': 'קובץ לא נמצא',
+};
